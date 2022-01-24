56db396980ecaa06db0cabcb1cc0c9debf289eec
Lucian Ticoiu <ticoiu.lucian@gmail.com>
2022-01-24 18:55:51 +0200

    Spring JPA part 1

--- src/main/java/com/example/demo/entity/Car.java
+++ src/main/java/com/example/demo/entity/Car.java
@@ -2,13 +2,24 @@ package com.example.demo.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
 public class Car {
 
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/demo/rest/MyFirstRestController.java
+++ src/main/java/com/example/demo/rest/MyFirstRestController.java
@@ -2,40 +2,59 @@ package com.example.demo.rest;
 
 import com.example.demo.entity.Car;
 import com.example.demo.repository.CarRepository;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class MyFirstRestController {
 
-    CarRepository carRepository = new CarRepository();
+    //TODO: Dependency injection
+    @Autowired
+    CarRepository carRepository;
 
     @GetMapping("/get-cars")
-    public List<Car> getCars() {
-        return carRepository.getAllCars();
+    public ResponseEntity<Iterable<Car>> getCars() {
+        return new ResponseEntity<>(carRepository.findAll(), HttpStatus.OK);
     }
 
+    //TODO: Optional
+    //TODO: lambda expressions
     @GetMapping("/get-car/{id}")
-    public Car getCarById(@PathVariable int id) {
-        return carRepository.getCarById(id);
+    public ResponseEntity<Car> getCarById(@PathVariable Long id) {
+        final Optional<Car> optionalCar = carRepository.findById(id);
+        return optionalCar.map(car -> new ResponseEntity<>(car, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
+    //TODO: use DTOs
+    //TODO: add logging
     @PostMapping("/create-car")
-    public void createCar(@RequestBody Car car) {
-        carRepository.addCar(car);
+    public ResponseEntity<Car> createCar(@RequestBody Car car) {
+        try {
+            return new ResponseEntity<>(carRepository.save(car), HttpStatus.OK);
+        } catch (DataIntegrityViolationException e) {
+            //TODO: use logger
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @DeleteMapping("/delete-car/{id}")
-    public void deleteCar(@PathVariable int id) {
-        carRepository.deleteCar(id);
-
-    }
-
-    @PutMapping("/update-car/{id}")
-    public void updateCar(@PathVariable int id, @RequestBody Car car) {
-        carRepository.updateCar(id, car);
-    }
+//    @DeleteMapping("/delete-car/{id}")
+//    public void deleteCar(@PathVariable int id) {
+//        carRepository.deleteCar(id);
+//
+//    }
+//
+//    @PutMapping("/update-car/{id}")
+//    public void updateCar(@PathVariable int id, @RequestBody Car car) {
+//        carRepository.updateCar(id, car);
+//    }
 }
